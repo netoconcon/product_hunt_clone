d04f8f9dc1e205dd9e118158c66173543979f4c5
Neto Concon <concon.neto2@gmail.com>
2020-07-09 12:03:12 -0300

    add unit test for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Fullname return first and last name capitalized" do
+    user = User.new(first_name: "RoBErto", last_name: "baRRos")
+    assert_equal 'Roberto Barros', user.fullname
+
+    # assert_equal "Resultado Esperado", lógica
+  end
 end
